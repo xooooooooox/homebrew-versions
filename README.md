4ba507a06c3dc49d06885d4db0a60d05b17c7b9e
xooooooooox <xozoz.sos@gmail.com>
2025-03-02 00:43:43 +0800

    feat: add hazel 5.3.6

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # homebrew-versions
+
+Old version brew file.
+
+## How to use
+
+> Take installing an older version of Hazel as an example.
+
+- step1: download `casks/h/hazel/5.3.6/hazel.rb` to folder `~/Downloads`
+- step2: `brew install ~/Downloads/hazel.rb`
---
+++ casks/h/hazel/5.3.6/hazel.rb
@@ -0,0 +1,32 @@
+cask "hazel" do
+  version "5.3.6"
+  sha256 "ad7729c1f3db15add28eab470b0d271594dbbe94ebf512178857574ffd0ff3b8"
+
+  url "https://s3.amazonaws.com/Noodlesoft/Hazel-#{version}.dmg",
+      verified: "s3.amazonaws.com/Noodlesoft/"
+  name "Hazel"
+  desc "Automated organisation"
+  homepage "https://www.noodlesoft.com/"
+
+  livecheck do
+    url "https://www.noodlesoft.com/Products/Hazel/generate-appcast.php"
+    strategy :sparkle
+  end
+
+  auto_updates true
+  depends_on macos: ">= :mojave"
+
+  app "Hazel.app"
+
+  uninstall quit: "86Z3GCJ4MF.com.noodlesoft.HazelHelper"
+
+  zap trash: [
+    "~/Library/Application Support/Hazel",
+    "~/Library/Caches/com.noodlesoft.HazelHelper",
+    "~/Library/Logs/Hazel",
+    "~/Library/Preferences/86Z3GCJ4MF.com.noodlesoft.HazelHelper.plist",
+    "~/Library/Preferences/com.noodlesoft.Hazel.plist",
+    "~/Library/Preferences/com.noodlesoft.HazelHelper.plist",
+    "~/Library/Saved Application State/com.noodlesoft.Hazel.savedState",
+  ]
+end
